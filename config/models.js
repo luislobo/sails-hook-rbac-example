978ba36b05d7dbe4feec8c3256989d479741432e
Luis Lobo Borobia <luislobo@gmail.com>
2021-06-20 22:39:51 -0500

    Change crteatedAt and updatedAt to ref and datetime to use native mongodb dates

--- config/models.js
+++ config/models.js
@@ -69,8 +69,8 @@ module.exports.models = {
   ***************************************************************************/
 
   attributes: {
-    createdAt: { type: 'number', autoCreatedAt: true, },
-    updatedAt: { type: 'number', autoUpdatedAt: true, },
+    createdAt: { type: 'ref', columnType: 'datetime', autoCreatedAt: true, },
+    updatedAt: { type: 'ref', columnType: 'datetime', autoUpdatedAt: true, },
     id: { type: 'string', columnName: '_id' },
     //--------------------------------------------------------------------------
     //  /\   Using MongoDB?
