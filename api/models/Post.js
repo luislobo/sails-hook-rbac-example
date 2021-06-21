f0332d8d6f6a60eafd0db82f50a718a8fe5e35fa
Luis Lobo Borobia <luislobo@gmail.com>
2021-06-20 23:27:39 -0500

    Add Post model

---
+++ api/models/Post.js
@@ -0,0 +1,39 @@
+/**
+ * Post.js
+ *
+ * A message post
+ */
+
+module.exports = {
+
+  attributes: {
+
+    //  ╔═╗╦═╗╦╔╦╗╦╔╦╗╦╦  ╦╔═╗╔═╗
+    //  ╠═╝╠╦╝║║║║║ ║ ║╚╗╔╝║╣ ╚═╗
+    //  ╩  ╩╚═╩╩ ╩╩ ╩ ╩ ╚╝ ╚═╝╚═╝
+
+    subject: {
+      type: 'string',
+      required: true,
+      maxLength: 500,
+      example: 'This is a subject',
+    },
+
+    //  ╔═╗╔╦╗╔╗ ╔═╗╔╦╗╔═╗
+    //  ║╣ ║║║╠╩╗║╣  ║║╚═╗
+    //  ╚═╝╩ ╩╚═╝╚═╝═╩╝╚═╝
+    // n/a
+
+    //  ╔═╗╔═╗╔═╗╔═╗╔═╗╦╔═╗╔╦╗╦╔═╗╔╗╔╔═╗
+    //  ╠═╣╚═╗╚═╗║ ║║  ║╠═╣ ║ ║║ ║║║║╚═╗
+    //  ╩ ╩╚═╝╚═╝╚═╝╚═╝╩╩ ╩ ╩ ╩╚═╝╝╚╝╚═╝
+
+    author: {
+      model: 'user',
+      required: true,
+      description: 'User that create the post',
+    },
+
+  },
+
+};
